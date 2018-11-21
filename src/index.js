927d90c69f15ed45bb2640854a2fb035d1b24829
dubenchuk6@gmail.com <dubenchuk6@gmail.com>
2018-11-21 13:21:49 +0200

    fix function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 function areIntersected (rect1, rect2) {
 	if (rect2.height && rect2.width) {
 		return ((rect2.left < (rect1.left + rect1.width) && rect2.left < (rect1.left + rect2.width))
-      && (rect2.top > (rect1.top - rect2.height)) && rect2.top < (rect1.top + rect1.height + rect2.height))
-      && ((rect2.left < (rect1.left + rect1.width) && rect2.left < (rect1.left + rect2.width))
-      && (rect2.top < (rect1.top + rect2.height)) && rect2.top < (rect1.top + rect1.height + rect2.height))
+      && (rect2.top > (rect1.top - rect2.height)) && rect2.top < (rect1.top + rect1.height + rect2.height)
+      || (rect2.top < (rect1.top + rect2.height)) && rect2.top < (rect1.top + rect1.height + rect2.height))
       && ((rect2.left < (rect1.left + rect1.width) && rect2.left > (rect1.left - rect2.width))
       && (rect2.top < (rect1.top + rect2.height)) && rect2.top < (rect1.top + rect1.height + rect2.height))
       && ((rect2.left < (rect1.left + rect1.width) && rect2.left > (rect1.left - rect2.width))
